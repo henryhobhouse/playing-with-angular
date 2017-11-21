35a55793c26977a65c6f715a252a0303feff1a48
henryhobhouse <henryhobhouse@gmail.com>
2017-11-21 11:08:15 +0000

    Added forms module and put two-way binding onto the input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Playing with Angular';
+  title = 'Playing with Angular...';
 }
